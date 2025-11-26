d85eec39a96c2e87ebdc9e9d4108b3654585de05
IsaacChua123 <isaaccbh888@gmail.com>
2025-11-26 21:29:38 +0800

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/MindTutor/",
-  root: ".",
+  root: "mindtutor",
   server: {
     port: 3000,
     host: true,
